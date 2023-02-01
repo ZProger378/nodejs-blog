267b0298b1a1d0b004f16edb3ae3736dcb81b22e
ZProger378 <hackm378@gmail.com>
2023-02-01 22:03:22 +0300

    Создал базовую структуру проекта и начал писать страницу

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// Import the fucking modules
 const express = require("express")
+const body_parser = require("body-parser")
+const router = require("./router")
 
-const app = express()
-app.listen(3000)
+const app = express()  // Initialization the fucking app
+
+const PORT = 3000
+app.use(router)
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+app.use(body_parser.urlencoded({'extended': false}))
+
+// Server polling...
+app.listen(PORT, () => {
+    console.log(`[i] Server has been started on port ${PORT}`)
+})
