b71644c1259d1d24de8579a34dfb399c93fa94ad
ZProger378 <hackm378@gmail.com>
2023-02-06 20:53:00 +0300

    Закончил регистрацию и обновил авторизацию для использования hash'а вместо пароля в виде plain text

--- router.js
+++ router.js
@@ -1,9 +1,16 @@
 // Импорт express
 const express = require("express")
+// Импорт crypto
+const crypto = require('crypto')
 
 // Импорт БД
 const sql = require("./db_config")
 const router = express.Router()  // Создание экземпляра роутера
+
+let create_hash = (name) => {
+    let hash = crypto.createHash('md5').update(name).digest('hex')
+    return hash
+}
 
 // Перенаправление и рендер страниц
 
@@ -23,7 +30,8 @@ router.post("/login", (req, res) => {
     let login = req.body.login
     let pass = req.body.pass
     let remember_me = req.body.remember_me
-    sql.query("SELECT * FROM users WHERE login = ? AND password = ?", [login, pass], (err, result) => {
+    let pass_hash = create_hash(pass)
+    sql.query("SELECT * FROM users WHERE login = ? AND password = ?", [login, pass_hash], (err, result) => {
         if (result[0] != undefined) {
             let chrs = 'abdehkmnpswxzABDEFGHKMNPQRSTWXZ123456789'
             let len = 16
@@ -32,7 +40,7 @@ router.post("/login", (req, res) => {
                 let pos = Math.floor(Math.random() * chrs.length)
                 token += chrs.substring(pos,pos+1)
             }
-            sql.query("INSERT INTO sessions (user_id, token) VALUES (?, ?)", [result[0].id, token])
+            sql.query("INSERT INTO sessions (user_id, token) VALUES (?, ?)", [result[0].login, token])
             res.cookie("token", token)
             res.send("success")
         } else
@@ -43,6 +51,19 @@ router.post("/reg", (req, res) => {
     let login = req.body.login
     let pass = req.body.pass
     let auto_auth = req.body.auto_auth
+    let pass_hash = create_hash(pass)
+    sql.query("INSERT INTO users (login, password) VALUES (?, ?)", [login, pass_hash])
+    if (auto_auth) {
+        let chrs = 'abdehkmnpswxzABDEFGHKMNPQRSTWXZ123456789'
+            let len = 16
+            let token = ''
+            for (let i = 0; i < len; i++) {
+                let pos = Math.floor(Math.random() * chrs.length)
+                token += chrs.substring(pos,pos+1)
+            }
+            sql.query("INSERT INTO sessions (user_id, token) VALUES (?, ?)", [login, token])
+            res.cookie("token", token)
+    }
 })
 
 module.exports = router
